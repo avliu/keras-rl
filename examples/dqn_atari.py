deac854fe6c3aa99c834c1c5b7196bcd862e5972
Alex Liu <avliu@buffalo.edu>
2019-10-15 10:14:21 -0400

    dqn_atari testing by timesteps

--- examples/dqn_atari.py
+++ examples/dqn_atari.py
@@ -2,6 +2,7 @@ from __future__ import division
 import argparse
 
 from PIL import Image
+import pandas as pd
 import numpy as np
 import gym
 
@@ -40,84 +41,102 @@ class AtariProcessor(Processor):
     def process_reward(self, reward):
         return np.clip(reward, -1., 1.)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--mode', choices=['train', 'test'], default='train')
-parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v4')
-parser.add_argument('--weights', type=str, default=None)
-args = parser.parse_args()
+def build_train_test(args, timesteps):
 
-# Get the environment and extract the number of actions.
-env = gym.make(args.env_name)
-np.random.seed(123)
-env.seed(123)
-nb_actions = env.action_space.n
+    # Get the environment and extract the number of actions.
+    env = gym.make(args.env_name)
+    np.random.seed(123)
+    env.seed(123)
+    nb_actions = env.action_space.n
 
-# Next, we build our model. We use the same model that was described by Mnih et al. (2015).
-input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
-model = Sequential()
-if K.image_dim_ordering() == 'tf':
-    # (width, height, channels)
-    model.add(Permute((2, 3, 1), input_shape=input_shape))
-elif K.image_dim_ordering() == 'th':
-    # (channels, width, height)
-    model.add(Permute((1, 2, 3), input_shape=input_shape))
-else:
-    raise RuntimeError('Unknown image_dim_ordering.')
-model.add(Convolution2D(32, (8, 8), strides=(4, 4)))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, (4, 4), strides=(2, 2)))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, (3, 3), strides=(1, 1)))
-model.add(Activation('relu'))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dense(nb_actions))
-model.add(Activation('linear'))
-print(model.summary())
+    # Next, we build our model. We use the same model that was described by Mnih et al. (2015).
+    input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
+    model = Sequential()
+    if K.image_dim_ordering() == 'tf':
+        # (width, height, channels)
+        model.add(Permute((2, 3, 1), input_shape=input_shape))
+    elif K.image_dim_ordering() == 'th':
+        # (channels, width, height)
+        model.add(Permute((1, 2, 3), input_shape=input_shape))
+    else:
+        raise RuntimeError('Unknown image_dim_ordering.')
+    model.add(Convolution2D(32, (8, 8), strides=(4, 4)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, (4, 4), strides=(2, 2)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, (3, 3), strides=(1, 1)))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
 
-# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-# even the metrics!
-memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
-processor = AtariProcessor()
+    # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+    # even the metrics!
+    memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
+    processor = AtariProcessor()
 
-# Select a policy. We use eps-greedy action selection, which means that a random action is selected
-# with probability eps. We anneal eps from 1.0 to 0.1 over the course of 1M steps. This is done so that
-# the agent initially explores the environment (high eps) and then gradually sticks to what it knows
-# (low eps). We also set a dedicated eps value that is used during testing. Note that we set it to 0.05
-# so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
-policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                              nb_steps=1000000)
+    # Select a policy. We use eps-greedy action selection, which means that a random action is selected
+    # with probability eps. We anneal eps from 1.0 to 0.1 over the course of 1M steps. This is done so that
+    # the agent initially explores the environment (high eps) and then gradually sticks to what it knows
+    # (low eps). We also set a dedicated eps value that is used during testing. Note that we set it to 0.05
+    # so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
+                                  nb_steps=1000000)
 
-# The trade-off between exploration and exploitation is difficult and an on-going research topic.
-# If you want, you can experiment with the parameters or use a different policy. Another popular one
-# is Boltzmann-style exploration:
-# policy = BoltzmannQPolicy(tau=1.)
-# Feel free to give it a try!
+    # The trade-off between exploration and exploitation is difficult and an on-going research topic.
+    # If you want, you can experiment with the parameters or use a different policy. Another popular one
+    # is Boltzmann-style exploration:
+    # policy = BoltzmannQPolicy(tau=1.)
+    # Feel free to give it a try!
 
-dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
-               processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
-               train_interval=4, delta_clip=1.)
-dqn.compile(Adam(lr=.00025), metrics=['mae'])
+    dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
+                   processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
+                   train_interval=4, delta_clip=1.)
+    dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
-if args.mode == 'train':
-    # Okay, now it's time to learn something! We capture the interrupt exception so that training
-    # can be prematurely aborted. Notice that now you can use the built-in Keras callbacks!
-    weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
-    checkpoint_weights_filename = 'dqn_' + args.env_name + '_weights_{step}.h5f'
-    log_filename = 'dqn_{}_log.json'.format(args.env_name)
-    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
-    callbacks += [FileLogger(log_filename, interval=100)]
-    dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000)
+    if args.mode == 'train':
+        # Okay, now it's time to learn something! We capture the interrupt exception so that training
+        # can be prematurely aborted. Notice that now you can use the built-in Keras callbacks!
+        weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
+        checkpoint_weights_filename = 'dqn_' + args.env_name + '_weights_{step}.h5f'
+        log_filename = 'dqn_{}_log.json'.format(args.env_name)
+        callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
+        callbacks += [FileLogger(log_filename, interval=100)]
+        dqn.fit(env, callbacks=callbacks, nb_steps=timesteps, log_interval=10000)
 
-    # After training is done, we save the final weights one more time.
-    dqn.save_weights(weights_filename, overwrite=True)
+        # After training is done, we save the final weights one more time.
+        dqn.save_weights(weights_filename, overwrite=True)
 
-    # Finally, evaluate our algorithm for 10 episodes.
-    dqn.test(env, nb_episodes=10, visualize=False)
-elif args.mode == 'test':
-    weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
-    if args.weights:
-        weights_filename = args.weights
-    dqn.load_weights(weights_filename)
-    dqn.test(env, nb_episodes=10, visualize=True)
+        # Finally, evaluate our algorithm for 10 episodes.
+        result = dqn.test(env, nb_episodes=10, visualize=False)
+    elif args.mode == 'test':
+        weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
+        if args.weights:
+            weights_filename = args.weights
+        dqn.load_weights(weights_filename)
+        result = dqn.test(env, nb_episodes=10, visualize=False)
+    return result
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', choices=['train', 'test'], default='train')
+    parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v4')
+    parser.add_argument('--weights', type=str, default=None)
+    parser.add_argument('--degree', type=int, default=7)
+    args = parser.parse_args()
+
+    degree = args.degree
+    timesteps = [10**i for i in range(2, degree)] + [5 * 10**i for i in range(2, degree)]
+    output = pd.Series(index=timesteps)
+    for t in timesteps:
+        result = build_train_test(args, t)
+        rewards = pd.Series(result.history['episode_reward'])
+        avg_reward = rewards.mean(0)
+        output[t] = avg_reward
+        print(f'average reward: {avg_reward}')
+
+    output.to_csv(f'{args.env_name}_results.csv', index_label=['timesteps'], header=['average reward'])
